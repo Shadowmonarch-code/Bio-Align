f57b42aec0503caa19c8b6778d1dc09ea1d5c599
Z User <z@container>
2026-08-12 11:02:20 +0000

    43a84a34-fcc8-41d8-9311-ab18574b78a2

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,11 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
+  allowedDevOrigins: [
+    "localhost:81",
+    "*.space-z.ai",
+    "preview-chat-ceb88da3-513f-4e96-a4ce-3e2909f9476f.space-z.ai",
+  ],
 };
 
 export default nextConfig;
